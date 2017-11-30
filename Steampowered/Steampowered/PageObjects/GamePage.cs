ed6aae97525efa0d64bd1a1f4fd2b47e9da0d07d
DESKTOP-ALU9E0R\user <san9-hovrin@mail.ru>
2017-12-01 01:50:05 +0300

    Fixed XPath for Game Page

--- Steampowered/Steampowered/PageObjects/GamePage.cs
+++ Steampowered/Steampowered/PageObjects/GamePage.cs
@@ -15,10 +15,14 @@ namespace Steampowered.PageObjects
             new Label(By.XPath("//div[@id='game_area_purchase']//div[contains(@class,'discount_original_price')]"), "lblOriginalPrice");
         private readonly Label _lblDiscountPrice = 
             new Label(By.XPath("//div[@id='game_area_purchase']//div[contains(@class,'discount_final_price')]"), "lblDiscountPrice");
-        private readonly Label _lblNameGame = 
+        private readonly Label _lblNameGame1 = 
             new Label(By.XPath("//div[contains(@class, 'game_title_area')]//div[contains(@class, 'apphub_AppName')]"), "lblNameGame");
+        private readonly Label _lblNameGame2 =
+            new Label(By.XPath("//div[contains(@class,'game_title_area')]/h2[contains(@class,'pageheader')]"), "lblNameGame");
         private readonly Button _btnInstallSteam = 
             new Button(By.XPath("//*[@id='global_action_menu']//a[contains(@class,'header_installsteam_btn_content')]"), "btnInstallSteam");
+        private GameInfo gameInfo;
+        private Label _lblNameGame;
 
         public GamePage()
         {
@@ -27,11 +31,21 @@ namespace Steampowered.PageObjects
 
         public GameInfo GetPriceAndDiscount()
         {
-            var gameInfo = new GameInfo(_lblNameGame.GetText(),
-                _lblDiscount.GetText().Trim(_charsToTrim),
-                _lblOriginalPrice.GetText().Trim(_charsToTrim),
-                _lblDiscountPrice.GetText().Trim(_charsToTrim));
+            if (_lblNameGame1.IsExistOnPage())
+            {
+                _lblNameGame = _lblNameGame1;
+            }
+            else if (_lblNameGame2.IsExistOnPage())
+            {
+                _lblNameGame = _lblNameGame2;
+            }
+
+            gameInfo = new GameInfo(_lblNameGame.GetText(),
+            _lblDiscount.GetText().Trim(_charsToTrim),
+            _lblOriginalPrice.GetText().Trim(_charsToTrim),
+            _lblDiscountPrice.GetText().Trim(_charsToTrim));
             return gameInfo;
+        
         }
 
         public void NavigateToDownloadSteam()
